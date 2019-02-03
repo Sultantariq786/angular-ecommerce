b1429baa249660eba86231bd426b759f37fbce7c
Sultantariq786 <newtechtariq@gmail.com>
2019-02-03 22:51:21 +0530

    fix prod build issue

--- server.js
+++ server.js
@@ -1,15 +1,8 @@
-//Install express server
 const express = require('express');
-const path = require('path');
-
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static('./dist/angular-ecommerce'));
+// Run the app by serving the static files in the dist directory
+app.use(express.static(__dirname + '/dist'));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname,'/dist/angular-ecommerce/index.html'));
-});
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
